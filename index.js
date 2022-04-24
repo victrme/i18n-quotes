444777f9287c3ebb8de9d7ea8e2a80323f65b586
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-04-24 11:33:12 +0200

    Migration to netlify functions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express')
+const serverless = require('serverless-http')
+const app = express()
+const router = express.Router()
+const bodyParser = require('body-parser')
+
 const quotes = {
 	en: require('./quotes/en.json'),
 	fr: require('./quotes/fr.json'),
@@ -9,24 +15,12 @@ const quotes = {
 
 const getLang = (lang) => Object.keys(quotes).find((l) => lang === l) || 'en'
 const randInt = (max) => Math.floor(Math.random() * max)
-const express = require('express')
-const cors = require('cors')
-const app = express()
 
-app.use(
-	cors({
-		credientials: true,
-		origin: true,
-	})
-)
-
-app.listen(process.env.PORT || 8080, () => console.log('server running on port 8080'))
-
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
 	res.status(200).send(quotes.en[randInt(quotes.en.length)])
 })
 
-app.get('/:lang', (req, res) => {
+router.get('/:lang', (req, res) => {
 	const lang = getLang(req.params.lang)
 	const all = quotes[lang]
 
@@ -45,13 +39,20 @@ app.get('/:lang', (req, res) => {
 	res.status(200).send(all[randInt(all.length)])
 })
 
-app.get('/:lang/all', (req, res) => {
+router.get('/:lang/all', (req, res) => {
 	res.status(200).send(quotes[getLang(req.params.lang)])
 })
 
-app.get('/:lang/count', (req, res) => {
+router.get('/:lang/count', (req, res) => {
 	const lang = getLang(req.params.lang)
 	const length = quotes[lang].length
 
 	res.status(200).send({ lang, length })
 })
+
+app.use(bodyParser.json())
+app.use('/.netlify/functions/server', router) // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')))
+
+module.exports = app
+module.exports.handler = serverless(app)

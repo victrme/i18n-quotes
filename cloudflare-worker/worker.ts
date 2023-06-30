6d169dc942aa2cdcb67f4c0200f6d15df53c44c8
Victor Azevedo <victor.azevedo@mail.com>
2023-06-30 15:15:07 +0200

    Working cloudflare worker

---
+++ cloudflare-worker/worker.ts
@@ -0,0 +1,41 @@
+import classic from './classic'
+import kaamelott from './kaamelott'
+import inspirobot from './inspirobot'
+
+export type Quote = {
+	author: string
+	content: string
+}
+
+export default {
+	async fetch(request: Request): Promise<Response> {
+		const pathname = new URL(request.url).pathname?.replace('/', '').split('/') ?? []
+		const lang = pathname[1] ?? 'en'
+		const type = pathname[0]
+		let result: Quote[]
+
+		switch (type) {
+			case 'kaamelott':
+				result = await kaamelott()
+				break
+
+			case 'inspirobot':
+				result = await inspirobot()
+				break
+
+			case 'classic':
+				result = await classic(lang)
+				break
+
+			default:
+				result = await classic('en')
+		}
+
+		return new Response(JSON.stringify(result), {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+			},
+		})
+	},
+}

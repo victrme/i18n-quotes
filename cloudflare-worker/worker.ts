39206d969811aad81799f95ef8025e82d7be20d1
Victor Azevedo <victor.azevedo@mail.com>
2023-10-24 09:27:46 +0200

    No need for imports in cf-workers

--- cloudflare-worker/worker.ts
+++ cloudflare-worker/worker.ts
@@ -1,10 +1,13 @@
-import classic from './classic'
-import kaamelott from './kaamelott'
-import inspirobot from './inspirobot'
-
-export type Quote = {
+type Quote = {
 	author: string
 	content: string
+}
+
+type Inspirobot = {
+	data: {
+		text?: string
+		type?: string
+	}[]
 }
 
 export default {
@@ -38,4 +41,61 @@ export default {
 			},
 		})
 	},
+}
+
+async function classic(lang: string): Promise<Quote[]> {
+	const quotes = {
+		en: async (): Promise<Quote[]> => import('../quotes/en.json'),
+		fr: async (): Promise<Quote[]> => import('../quotes/fr.json'),
+		de: async (): Promise<Quote[]> => import('../quotes/de.json'),
+		it: async (): Promise<Quote[]> => import('../quotes/it.json'),
+		nl: async (): Promise<Quote[]> => import('../quotes/nl.json'),
+		pl: async (): Promise<Quote[]> => import('../quotes/pl.json'),
+		ru: async (): Promise<Quote[]> => import('../quotes/ru.json'),
+		sv: async (): Promise<Quote[]> => import('../quotes/sv.json'),
+	}
+
+	const list: Quote[] = (await quotes[lang]()).default
+	const result: Quote[] = []
+
+	for (let i = 0; i < 20; i++) {
+		result.push(list[Math.floor(Math.random() * list.length)])
+	}
+
+	return result
+}
+
+async function inspirobot(): Promise<Quote[]> {
+	let inspi: Inspirobot['data'] = []
+	let result: Quote[] = []
+
+	const filtering = (quote: string) => !quote.includes('[pause') || quote.length < 200
+
+	try {
+		const resp = await fetch('https://inspirobot.me/api?generateFlow=1')
+		const json = (await resp.json()) as Inspirobot
+		inspi = json.data
+	} catch (error) {
+		console.warn("Can't get to inspirobot: ", error)
+	}
+
+	try {
+		inspi = inspi.filter((d) => d.type === 'quote' && filtering(d.text ?? ''))
+		result = inspi.map((d) => ({ author: 'Inspirobot', content: d.text ?? '' }))
+	} catch (error) {
+		console.log(error)
+	}
+
+	return result
+}
+
+async function kaamelott(): Promise<Quote[]> {
+	let list = (await import('../quotes/kaamelott.json')).default
+	let result: Quote[] = []
+
+	for (let i = 0; i < 20; i++) {
+		result.push(list[Math.floor(Math.random() * list.length)])
+	}
+
+	return result
 }
